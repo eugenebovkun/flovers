7c9d1a9c26cf6d6e3fc5915752b06c3c8388fbd3
evgeniy.bovkun <evgeniy.bovkun@gmail.com>
2019-07-31 11:17:43 +0300

    initial elements

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('svg', { static: false }) svg: ElementRef;
 
-  constructor() {}
+  constructor() {
+
+  }
+
+  public drawOnCanvas() {
+    const drawing = this.canvas.nativeElement.getContext('2d');
+    drawing.fillRect(10, 10, 20, 20);
+    drawing.fillRect(50, 10, 20, 20);
+    drawing.fillRect(10, 50, 20, 20);
+    drawing.fillRect(30, 30, 20, 20);
+    drawing.fillRect(50, 50, 20, 20);
+  }
+
+  public drawOnSVG() {}
 
 }
